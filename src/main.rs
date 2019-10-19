16d0a6f56b2ccd134984110a8390bc57d1116991
Ryo Yamashita <qryxip@gmail.com>
2019-10-19 16:21:59 +0900

    Add some options

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,3 @@
-use cargo_linked::util::CompileOptionsForSingleTargetArgs;
-
 use cargo::core::shell::Shell;
 use failure::Fallible;
 use structopt::clap::AppSettings;
@@ -9,7 +7,7 @@ use std::io::{self, Write as _};
 use std::path::PathBuf;
 
 fn main() {
-    let opt = Opt::from_args();
+    let Opt::Linked(opt) = Opt::from_args();
     let mut config = cargo::Config::default()
         .unwrap_or_else(|e| cargo::exit_with_error(e.into(), &mut Shell::new()));
     if let Err(err) = opt.run(&mut config) {
@@ -21,92 +19,124 @@ fn main() {
 #[structopt(
     author,
     about,
-    bin_name = "cargo",
+    bin_name("cargo"),
     global_settings(&[AppSettings::ColoredHelp, AppSettings::DeriveDisplayOrder])
 )]
 enum Opt {
     #[structopt(author, about, name = "linked")]
-    Linked {
-        #[structopt(long, help = "Run in debug mode")]
-        debug: bool,
-        #[structopt(long, help = "Target the `lib`")]
-        lib: bool,
-        #[structopt(
-            long,
-            value_name = "NAME",
-            conflicts_with_all(&["lib", "example", "test", "bench"]),
-            help = "Target `bin`"
-        )]
-        bin: Option<String>,
-        #[structopt(
-            long,
-            value_name = "NAME",
-            conflicts_with_all(&["lib", "bin", "example", "bench"]),
-            help = "Target `test`"
-        )]
-        test: Option<String>,
-        #[structopt(
-            long,
-            value_name = "NAME",
-            conflicts_with_all(&["lib", "bin", "example", "test"]),
-            help = "Target `bench`"
-        )]
-        bench: Option<String>,
-        #[structopt(
-            long,
-            value_name = "NAME",
-            conflicts_with_all(&["lib", "bin", "test", "bench"]),
-            help = "Target `example`"
-        )]
-        example: Option<String>,
-        #[structopt(
-            long,
-            value_name = "PATH",
-            parse(from_os_str),
-            help = "Path to Cargo.toml"
-        )]
-        manifest_path: Option<PathBuf>,
-        #[structopt(
-            long,
-            value_name("WHEN"),
-            default_value("auto"),
-            possible_values(&["auto", "always", "never"]),
-            help("Coloring")
-        )]
-        color: String,
-    },
+    Linked(OptLinked),
 }
 
-impl Opt {
+#[derive(Debug, StructOpt)]
+struct OptLinked {
+    #[structopt(long, help("Target the `lib`"))]
+    lib: bool,
+    #[structopt(long, help("Run in debug mode"))]
+    debug: bool,
+    #[structopt(long, help("Activate all available features"))]
+    all_features: bool,
+    #[structopt(long, help("Do not activate the `default` config"))]
+    no_default_features: bool,
+    #[structopt(long, help("Require Cargo.lock and cache are up to date"))]
+    frozen: bool,
+    #[structopt(long, help("Require Cargo.lock is up to date"))]
+    locked: bool,
+    #[structopt(long, help("Run without accessing the network"))]
+    offline: bool,
+    #[structopt(
+        short,
+        long,
+        value_name("N"),
+        help("Number of parallel jobs, defaults to # of CPUs")
+    )]
+    jobs: Option<String>,
+    #[structopt(
+        long,
+        value_name("NAME"),
+        conflicts_with_all(&["lib", "example", "test", "bench"]),
+        help("Target the `bin`")
+    )]
+    bin: Option<String>,
+    #[structopt(
+        long,
+        value_name("NAME"),
+        conflicts_with_all(&["lib", "bin", "example", "bench"]),
+        help("Target the `test`")
+    )]
+    test: Option<String>,
+    #[structopt(
+        long,
+        value_name("NAME"),
+        conflicts_with_all(&["lib", "bin", "example", "test"]),
+        help("Target the `bench`")
+    )]
+    bench: Option<String>,
+    #[structopt(
+        long,
+        value_name("NAME"),
+        conflicts_with_all(&["lib", "bin", "test", "bench"]),
+        help("Target the `example`")
+    )]
+    example: Option<String>,
+    #[structopt(
+        long,
+        value_name("FEATURES"),
+        min_values(1),
+        help("Space-separated list of features to activate")
+    )]
+    features: Vec<String>,
+    #[structopt(long, value_name("PATH"), help("Path to Cargo.toml"))]
+    manifest_path: Option<PathBuf>,
+    #[structopt(long, value_name("WHEN"), help("Coloring: auto, always, never"))]
+    color: Option<String>,
+}
+
+impl OptLinked {
     fn run(&self, config: &mut cargo::Config) -> Fallible<()> {
-        let Self::Linked {
-            debug,
+        let Self {
             lib,
+            debug,
+            all_features,
+            no_default_features,
+            frozen,
+            locked,
+            offline,
+            jobs,
             bin,
             test,
             bench,
             example,
+            features,
             manifest_path,
             color,
         } = self;
 
-        cargo_linked::util::configure(config, manifest_path, color, |target_dir| {
-            target_dir.join("cargo_linked").join("target")
-        })?;
+        cargo_linked::util::Configure {
+            manifest_path,
+            color,
+            frozen: *frozen,
+            locked: *locked,
+            offline: *offline,
+            modify_target_dir: |d| d.join("cargo_linked").join("target"),
+        }
+        .configure(config)?;
 
         let ws = cargo_linked::util::workspace(config, manifest_path)?;
-        let (compile_options, target) = cargo_linked::util::compile_options_for_single_target(
-            CompileOptionsForSingleTargetArgs {
-                ws: &ws,
-                debug: *debug,
-                lib: *lib,
-                bin,
-                test,
-                bench,
-                example,
-                manifest_path,
-            },
-        )?;
+        let (compile_options, target) = cargo_linked::util::CompileOptionsForSingleTarget {
+            ws: &ws,
+            jobs,
+            lib: *lib,
+            bin,
+            test,
+            bench,
+            example,
+            debug: *debug,
+            features,
+            all_features: *all_features,
+            no_default_features: *no_default_features,
+            manifest_path,
+        }
+        .compile_options_for_single_target()?;
 
         let outcome = cargo_linked::LinkedPackages::find(&ws, &compile_options, target)?;
         let outcome = miniserde::json::to_string(&outcome);
--- src/util/mod.rs
+++ src/util/mod.rs
@@ -12,37 +12,52 @@ use std::ffi::{OsStr, OsString};
 use std::ops::Deref;
 use std::path::PathBuf;
 
-pub fn configure<F: FnOnce(PathBuf) -> PathBuf>(
-    config: &mut Config,
-    manifest_path: &Option<PathBuf>,
-    color: &str,
-    modify_target_dir: F,
-) -> crate::Result<()> {
-    let mut args = hashmap!();
-    if let Some(manifest_path) = manifest_path {
-        args.insert("manifest-path", vec![OsString::from(manifest_path)]);
+pub struct Configure<'a, F: FnOnce(PathBuf) -> PathBuf> {
+    pub manifest_path: &'a Option<PathBuf>,
+    pub color: &'a Option<String>,
+    pub frozen: bool,
+    pub locked: bool,
+    pub offline: bool,
+    pub modify_target_dir: F,
+}
+
+impl<F: FnOnce(PathBuf) -> PathBuf> Configure<'_, F> {
+    pub fn configure(self, config: &mut Config) -> crate::Result<()> {
+        let Self {
+            manifest_path,
+            color,
+            frozen,
+            locked,
+            offline,
+            modify_target_dir,
+        } = self;
+
+        let mut args = hashmap!();
+        if let Some(manifest_path) = manifest_path {
+            args.insert("manifest-path", vec![manifest_path.into()]);
+        }
+
+        let target_dir = arg_matches_from(args)
+            .workspace(&config)
+            .with_context(|_| crate::ErrorKind::Cargo)?
+            .target_dir()
+            .into_path_unlocked();
+        let target_dir = modify_target_dir(target_dir);
+
+        config
+            .configure(
+                0,
+                None,
+                color,
+                frozen,
+                locked,
+                offline,
+                &Some(target_dir),
+                &[],
+            )
+            .with_context(|_| crate::ErrorKind::Cargo)
+            .map_err(Into::into)
     }
-
-    let target_dir = arg_matches_from(args)
-        .workspace(&config)
-        .with_context(|_| crate::ErrorKind::Cargo)?
-        .target_dir()
-        .into_path_unlocked();
-    let target_dir = modify_target_dir(target_dir);
-
-    config
-        .configure(
-            0,
-            None,
-            &Some(color.to_owned()),
-            false,
-            false,
-            false,
-            &Some(target_dir),
-            &[],
-        )
-        .with_context(|_| crate::ErrorKind::Cargo)
-        .map_err(Into::into)
 }
 
 pub fn workspace<'a>(
@@ -61,100 +76,123 @@ pub fn workspace<'a>(
 }
 
 #[derive(Clone, Copy, Debug)]
-pub struct CompileOptionsForSingleTargetArgs<'a, 'b> {
+pub struct CompileOptionsForSingleTarget<'a, 'b> {
     pub ws: &'a Workspace<'a>,
-    pub debug: bool,
+    pub jobs: &'b Option<String>,
     pub lib: bool,
     pub bin: &'b Option<String>,
     pub test: &'b Option<String>,
     pub bench: &'b Option<String>,
     pub example: &'b Option<String>,
+    pub debug: bool,
+    pub features: &'b [String],
+    pub all_features: bool,
+    pub no_default_features: bool,
     pub manifest_path: &'b Option<PathBuf>,
 }
 
-pub fn compile_options_for_single_target<'a>(
-    args: CompileOptionsForSingleTargetArgs<'a, '_>,
-) -> crate::Result<(CompileOptions<'a>, &'a Target)> {
-    let CompileOptionsForSingleTargetArgs {
-        ws,
-        debug,
-        lib,
-        bin,
-        test,
-        bench,
-        example,
-        manifest_path,
-    } = args;
+impl<'a> CompileOptionsForSingleTarget<'a, '_> {
+    pub fn compile_options_for_single_target(
+        self,
+    ) -> crate::Result<(CompileOptions<'a>, &'a Target)> {
+        let Self {
+            ws,
+            jobs,
+            lib,
+            bin,
+            test,
+            bench,
+            example,
+            debug,
+            features,
+            all_features,
+            no_default_features,
+            manifest_path,
+        } = self;
 
-    let mut args = hashmap!();
-    if let Some(manifest_path) = manifest_path {
-        args.insert("manifest-path", vec![OsString::from(manifest_path)]);
-    }
+        let mut args = hashmap!();
+        if let Some(jobs) = jobs {
+            args.insert("jobs", vec![jobs.into()]);
+        }
+        if !features.is_empty() {
+            args.insert("features", features.iter().map(Into::into).collect());
+        }
+        if all_features {
+            args.insert("all-features", vec![]);
+        }
+        if no_default_features {
+            args.insert("no-default-features", vec![]);
+        }
+        if let Some(manifest_path) = manifest_path {
+            args.insert("manifest-path", vec![manifest_path.into()]);
+        }
 
-    let current = ws.current().with_context(|_| crate::ErrorKind::Cargo)?;
+        let current = ws.current().with_context(|_| crate::ErrorKind::Cargo)?;
 
-    let find_by_name = |name: &str, kind: &'static str| -> _ {
-        current
-            .targets()
-            .iter()
-            .find(|t| t.name() == name && t.kind().description() == kind)
-            .ok_or_else(|| {
-                crate::Error::from(crate::ErrorKind::NoSuchTarget {
-                    kind,
-                    name: Some(name.to_owned()),
+        let find_by_name = |name: &str, kind: &'static str| -> _ {
+            current
+                .targets()
+                .iter()
+                .find(|t| t.name() == name && t.kind().description() == kind)
+                .ok_or_else(|| {
+                    crate::Error::from(crate::ErrorKind::NoSuchTarget {
+                        kind,
+                        name: Some(name.to_owned()),
+                    })
                 })
-            })
-    };
-
-    if !debug {
-        args.insert("release", vec![]);
-    }
-
-    let (arg_key, arg_val, target) = if lib {
-        let target = current
-            .targets()
-            .iter()
-            .find(|t| t.is_lib())
-            .ok_or_else(|| crate::ErrorKind::NoSuchTarget {
-                kind: "lib",
-                name: None,
-            })?;
-        ("lib", vec![], target)
-    } else if let Some(bin) = bin {
-        let target = find_by_name(bin, "bin")?;
-        ("bin", vec![OsString::from(bin)], target)
-    } else if let Some(test) = test {
-        let target = find_by_name(test, "integration-test")?;
-        ("test", vec![OsString::from(test)], target)
-    } else if let Some(bench) = bench {
-        let target = find_by_name(bench, "bench")?;
-        ("bench", vec![OsString::from(bench)], target)
-    } else if let Some(example) = example {
-        let target = find_by_name(example, "example")?;
-        ("example", vec![OsString::from(example)], target)
-    } else {
-        let bins = current
-            .targets()
-            .iter()
-            .filter(|t| *t.kind() == TargetKind::Bin)
-            .collect::<Vec<_>>();
-        let target = if bins.len() == 1 {
-            &bins[0]
-        } else {
-            let name = current
-                .manifest()
-                .default_run()
-                .ok_or_else(|| crate::ErrorKind::AmbiguousTarget)?;
-            find_by_name(name, "bin")?
         };
-        ("bin", vec![OsString::from(target.name())], target)
-    };
 
-    args.insert(arg_key, arg_val);
-    let compile_options = arg_matches_from(args)
-        .compile_options(ws.config(), CompileMode::Build, Some(ws))
-        .with_context(|_| crate::ErrorKind::Cargo)?;
-    Ok((compile_options, target))
+        if !debug {
+            args.insert("release", vec![]);
+        }
+
+        let (arg_key, arg_val, target) = if lib {
+            let target = current
+                .targets()
+                .iter()
+                .find(|t| t.is_lib())
+                .ok_or_else(|| crate::ErrorKind::NoSuchTarget {
+                    kind: "lib",
+                    name: None,
+                })?;
+            ("lib", vec![], target)
+        } else if let Some(bin) = bin {
+            let target = find_by_name(bin, "bin")?;
+            ("bin", vec![OsString::from(bin)], target)
+        } else if let Some(test) = test {
+            let target = find_by_name(test, "integration-test")?;
+            ("test", vec![OsString::from(test)], target)
+        } else if let Some(bench) = bench {
+            let target = find_by_name(bench, "bench")?;
+            ("bench", vec![OsString::from(bench)], target)
+        } else if let Some(example) = example {
+            let target = find_by_name(example, "example")?;
+            ("example", vec![OsString::from(example)], target)
+        } else {
+            let bins = current
+                .targets()
+                .iter()
+                .filter(|t| *t.kind() == TargetKind::Bin)
+                .collect::<Vec<_>>();
+            let target = if bins.len() == 1 {
+                &bins[0]
+            } else {
+                let name = current
+                    .manifest()
+                    .default_run()
+                    .ok_or_else(|| crate::ErrorKind::AmbiguousTarget)?;
+                find_by_name(name, "bin")?
+            };
+            ("bin", vec![OsString::from(target.name())], target)
+        };
+
+        args.insert(arg_key, arg_val);
+
+        let compile_options = arg_matches_from(args)
+            .compile_options(ws.config(), CompileMode::Build, Some(ws))
+            .with_context(|_| crate::ErrorKind::Cargo)?;
+        Ok((compile_options, target))
+    }
 }
 
 fn arg_matches_from(map: HashMap<&'static str, Vec<OsString>>) -> impl ArgMatchesExt {
